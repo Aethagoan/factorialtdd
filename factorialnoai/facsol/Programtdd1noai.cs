af6ca717f798ab3bde59ad208e79cc26bd0d66ee
aethagoan <aethagoan@outlook.com>
2025-09-09 18:02:34 -0600

    added stuff

---
+++ factorialnoai/facsol/Programtdd1noai.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace facsol;
+
+public class factorial {
+    // public static long compute(int n) {
+    //     if (n < 0) throw new Exception("negative");
+    //     if (n == 0) return 1;
+    //     return n * compute(n-1);
+    // }
+
+    public static long compute(int n) {
+        if (n < 0) throw new Exception("negative");
+        if (n == 0) return 1;
+        long acc = 1;
+        for (int i = n; i > 0; i--) {
+            acc *= i;
+        }
+        return acc;
+    }
+
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        int? n = null;
+       
+        if (args.Length > 0) {
+            try {
+                n = int.Parse(args[0]);
+            } catch {
+                while (n == null) {
+                    Console.Write("Input an int>\n");
+                    try {
+                        n = int.Parse(Console.ReadLine());
+
+                    } catch (Exception e) {
+                        continue;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        } else {
+            while (n == null) {
+                    Console.Write("Input an int>\n");
+                    try {
+                        n = int.Parse(Console.ReadLine());
+
+                    } catch (Exception e) {
+                        continue;
+                    }
+                    Console.WriteLine();
+                }
+        }
+       
+
+        string outstring = "";
+        for (int i = 0; i < n+1; i++){
+            outstring += $"{factorial.compute(i)}, {i}\n";
+        }
+
+        Console.WriteLine(outstring);
+
+        // ugh why is it like this....
+        using (StreamWriter writer = new("writeoutfile.txt")) {
+            writer.WriteLine(outstring);
+        }
+    }
+}
---
+++ factorialnoai/factests/facteststdd1noai.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+
+namespace facsol;
+
+public class UnitTests
+{
+    [Fact]
+    public void negativethrowserror()
+    {
+        try{
+            factorial.compute(-1);
+        }
+        catch (Exception e) {
+            Assert.True(e.Message == "negative");
+        }
+    }
+
+    [Fact]
+    public void zerogivesone() {
+        Assert.True(factorial.compute(0) == 1);
+    }
+
+    [Fact]
+    public void onegivesone() {
+        Assert.True(factorial.compute(1) == 1);
+    }
+
+    [Fact]
+    public void twogivestwo() {
+        Assert.True(factorial.compute(2) == 2);
+    }
+
+    [Fact]
+    public void threegivessix() {
+        Assert.True(factorial.compute(3) == 6);
+    }
+}
